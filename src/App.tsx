693e423db5e57fd12dbf5452bee0dd63a291883b
Andrei <yantsevich92@gmail.com>
2022-01-29 15:45:57 +0300

    completed handle error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles/App.css'
 import PostList, {PostType} from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,12 +6,28 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/Modal/MyModal";
 import MyButton from "./components/UI/Button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./api/PostService";
+import Loader from './components/UI/Loader/Loader';
+import {useFetching} from "./hooks/useFetching";
 
 function App() {
     const [posts, setPosts] = useState<Array<PostType>>([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
+    const [totalCount, setTotalCount] = useState<string | number>(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
     const sortedAddSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+    const [fetching, isLoading, error] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        setTotalCount(response.headers['x-total-count'])
+    })
+
+    useEffect(() => {
+        fetching()
+    }, [])
 
     const createPost = (newPost: PostType) => {
         setPosts([...posts, newPost])
@@ -32,7 +48,12 @@ function App() {
             </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
-            <PostList posts={sortedAddSearchedPosts} title={'Posts about JS'} deletePost={deletePost}/>
+            {error && <h1>Error message ${error}</h1>}
+            {isLoading
+                ? <div style={{display: "flex", justifyContent: "center", marginTop: '50px'}}><Loader/></div>
+                : <PostList posts={sortedAddSearchedPosts} title={'Posts about JS'} deletePost={deletePost}/>
+            }
+
         </div>
     );
 }
